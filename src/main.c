429102014725ee29c1d6f49a87ef2adeb547742e
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2024-10-01 21:43:01 +0200

    add prototype of .ppm generation

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -14,23 +15,27 @@ main(void)
 	// const uint64_t max = 1920 * 1080 * 1000;
 	// const uint64_t max = (UINT64_MAX - 1) / 1000000000;
 	// const uint64_t max = (UINT64_MAX - 1);
-	const uint64_t wh = 21;
+	const uint64_t wh = 2001;
 	const uint64_t max = wh * wh;
 	const struct SoeCache cache = soe_init(max);
 	// (void)soe_is_prime(cache, max);
 	// dbg_assert(soe_is_prime(cache, 2111) == true);
+	FILE *file = fopen("test.ppm", "w");
+	dbg_assert(file);
+	fputs("P3\n", file);
+	fprintf(file, "%lu %lu\n", wh, wh);
+	fputs("225\n", file);
 	for (uint64_t i = 0; i < wh; ++i) {
 		for (uint64_t j = 0; j < wh; ++j) {
 			const int64_t x = spr_screen_to_coord_x(j, wh);
 			const int64_t y = spr_screen_to_coord_y(i, wh);
 			const uint64_t val = spr_coords_to_val(x,y);
 			const bool prime = soe_is_prime(cache, val);
-			const char sign = prime ? 'X' : '.';
-			putchar(' ');
-			putchar(sign);
+			const char* fmt = prime ?  "  0   0   0\n" : "255 255 255\n";
+			fputs(fmt, file);
 		}
-		putchar('\n');
 	}
+	fclose(file);
 	soe_deinit(cache);
 	return EXIT_SUCCESS;
 }
